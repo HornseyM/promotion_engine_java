ed35aec2bae6ab1f5f11a95fcb815c3031cee1f6
Matthew Hornsey <hornseym@live.co.uk>
2022-01-30 19:48:36 +0000

    Updated implementation of apply method for Promotion A to pass tests.

--- src/com/hornseym/prom_engine/main/java/PromotionA.java
+++ src/com/hornseym/prom_engine/main/java/PromotionA.java
@@ -1,5 +1,9 @@
 package com.hornseym.prom_engine.main.java;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PromotionA {
     /**
      * Promotion A is: 3 of A's for 130
@@ -18,7 +22,26 @@ public class PromotionA {
      */
     public static Basket apply(Basket basket)
     {
-        return basket;
+        // By default, return the basket unchanged
+        Basket ret = basket;
+        List<Item> items = basket.getItems();
+
+        // Get the As...
+        List<Item> threeAs = items.stream().filter((item) -> item.getName() == "A").limit(3).collect(Collectors.toList());
+
+        // ...And if there were 3, apply the promotion
+        if(threeAs.size() == 3)
+        {
+            // Get the items without the As the promotion is being applied to
+            items = items.stream().filter((item) -> !threeAs.contains(item)).collect(Collectors.toList());
+            // Create the promotion item and add it to the remaining items, then create a new basket.
+            Item promo = new Item("Three As", 130);
+            items.add(promo);
+            ret = new Basket(items);
+        }
+
+        // Return the old or new basket
+        return ret;
     }
     
 }
