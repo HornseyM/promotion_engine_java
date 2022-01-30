678529d20553c20f0db3604bcb866d9256880cf1
Matthew Hornsey <hornseym@live.co.uk>
2022-01-30 17:29:38 +0000

    Added copyright notices.

--- src/com/hornseym/prom_engine/main/java/Item.java
+++ src/com/hornseym/prom_engine/main/java/Item.java
@@ -1,5 +1,14 @@
 package com.hornseym.prom_engine.main.java;
 
+/* 
+ * Copyright (C) 2022  Matthew Hornsey
+
+ * This is part of the Promotion Engine project, relased under the terms of the 
+ * GNU General Public License as published bythe Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version. A copy of this
+ * is available at the root of the project or at <https://www.gnu.org/licenses/>.
+ */
+
 public class Item {
     /**
      * This class contains a item that can be in a basket.
--- src/com/hornseym/prom_engine/test/java/TestItem.java
+++ src/com/hornseym/prom_engine/test/java/TestItem.java
@@ -6,6 +6,15 @@ import org.junit.Test;
 
 import com.hornseym.prom_engine.main.java.Item;
 
+/* 
+ * Copyright (C) 2022  Matthew Hornsey
+
+ * This is part of the Promotion Engine project, relased under the terms of the 
+ * GNU General Public License as published bythe Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version. A copy of this
+ * is available at the root of the project or at <https://www.gnu.org/licenses/>.
+ */
+
 public class TestItem {
     /**
      * This is the test class encapsulating the unit tests for the Item class.
