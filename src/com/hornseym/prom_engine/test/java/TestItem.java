23483088fabf31b7e7c2243ad0d3116d35342df9
Matthew Hornsey <hornseym@live.co.uk>
2022-01-30 17:20:55 +0000

    Added test for name and price on Item class.

--- src/com/hornseym/prom_engine/test/java/TestItem.java
+++ src/com/hornseym/prom_engine/test/java/TestItem.java
@@ -1,5 +1,6 @@
 package com.hornseym.prom_engine.test.java;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
@@ -16,9 +17,28 @@ public class TestItem {
         /**
          * Test that the Item class can be initialise without issues
          */
-        Item item = new Item();
+        Item item = new Item("A", 50);
         assertNotNull(item);
-        
+    }
+
+    @Test
+    public void testItemInitialisedWithName()
+    {
+        /**
+         * Test that when the item is initialise with a name, it stores and returns the name.
+         */
+        Item item = new Item("A", 50);
+        assertEquals("A", item.getName());
+    }
+
+    @Test
+    public void testItemInitialisedWithPrice()
+    {
+        /**
+         * Test that when the item is initialise with a price, it stores and returns the price.
+         */
+        Item item = new Item("A", 50);
+        assertEquals(50, item.getPrice());
     }
     
 }
