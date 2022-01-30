61476dd7d54209a39f8c3473c9d89618108d7b10
Matthew Hornsey <hornseym@live.co.uk>
2022-01-30 17:13:39 +0000

    Saved the Item test file this time.

---
+++ src/com/hornseym/prom_engine/test/java/TestItem.java
@@ -0,0 +1,23 @@
+package com.hornseym.prom_engine.test.java;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class TestItem {
+    /**
+     * This is the test class encapsulating the unit tests for the Item class.
+     */
+
+    @Test
+    public void testItemInstantiable()
+    {
+        /**
+         * Test that the Item class can be initialise without issues
+         */
+        Item item = new Item();
+        assertNotNull(item);
+        
+    }
+    
+}
