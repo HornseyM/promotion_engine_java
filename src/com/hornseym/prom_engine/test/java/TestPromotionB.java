9b234f5a7c164ce226c43ee6938a7409dfdd1a7f
Matthew Hornsey <hornseym@live.co.uk>
2022-01-30 20:12:17 +0000

    Added further tests fro how Promotion B should work.

--- src/com/hornseym/prom_engine/test/java/TestPromotionB.java
+++ src/com/hornseym/prom_engine/test/java/TestPromotionB.java
@@ -3,6 +3,8 @@ package com.hornseym.prom_engine.test.java;
 import static org.junit.Assert.assertEquals;
 
 import com.hornseym.prom_engine.main.java.Basket;
+import com.hornseym.prom_engine.main.java.Item;
+import com.hornseym.prom_engine.main.java.ItemBuilder;
 import com.hornseym.prom_engine.main.java.PromotionB;
 
 import org.junit.Test;
@@ -30,5 +32,41 @@ public class TestPromotionB {
         Basket basket = new Basket();
         basket = PromotionB.apply(basket);
         assertEquals("An empty basket should be unchanged by promotion B", 0, basket.getTotal());
+    }
+
+    @Test
+    public void testBasketWithOneBIsUnchanged()
+    {
+        /**
+         * A basket only one B shoud be unchanged by applying Promotion B
+         */
+        Item[] items = {ItemBuilder.getB()};
+        Basket basket = new Basket(items);
+        basket = PromotionB.apply(basket);
+        assertEquals("A basket with just one B should be unchanged by promotion B", 30, basket.getTotal());
+    }
+
+    @Test
+    public void testBasketWithOneBAAndOthersIsUnchanged()
+    {
+        /**
+         * A basket only one As and other items shoud be unchanged by applying Promotion B
+         */
+        Item[] items = {ItemBuilder.getA(), ItemBuilder.getA(), ItemBuilder.getB()};
+        Basket basket = new Basket(items);
+        basket = PromotionB.apply(basket);
+        assertEquals("A basket with just one B and others should be unchanged by promotion B", 130, basket.getTotal());
+    }
+
+    @Test
+    public void testBasketWithTwoBsIsChanged()
+    {
+        /**
+         * A basket two Bs shoud be changed by applying Promotion b
+         */
+        Item[] items = {ItemBuilder.getB(), ItemBuilder.getB()};
+        Basket basket = new Basket(items);
+        basket = PromotionB.apply(basket);
+        assertEquals("A basket with two B should be undated by promotion B", 45, basket.getTotal());
     }
 }
