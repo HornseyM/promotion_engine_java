e650caed595d68a4a89e928c2fd57f321bf57e2f
Matthew Hornsey <hornseym@live.co.uk>
2022-01-30 18:05:29 +0000

    Added test for initialising the basket with different number of items.

--- src/com/hornseym/prom_engine/test/java/TestBasket.java
+++ src/com/hornseym/prom_engine/test/java/TestBasket.java
@@ -2,9 +2,13 @@ package com.hornseym.prom_engine.test.java;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import com.hornseym.prom_engine.main.java.Basket;
+import com.hornseym.prom_engine.main.java.Item;
 
 /* 
  * Copyright (C) 2022  Matthew Hornsey
@@ -21,12 +25,79 @@ public class TestBasket {
      */
 
     @Test
-    public void testBasketInitialisable()
+    public void testBasketInitialisableWithNoBasket()
     {
         /**
          * Test that the Basket class can be initialise without issues
          */
         Basket basket = new Basket();
+        assertNotNull(basket);
+    }
+
+    @Test
+    public void testBasketInitialisableWithEmptyBasketArray()
+    {
+        /**
+         * Test that the Basket class can be initialise with nothing in it
+         */
+        Item[] items = {};
+        Basket basket = new Basket(items);
+        assertNotNull(basket);
+    }
+
+    @Test
+    public void testBasketInitialisableWithEmptyBasketArrayList()
+    {
+        /**
+         * Test that the Basket class can be initialise with nothing in it
+         */
+        Basket basket = new Basket(new ArrayList<Item>());
+        assertNotNull(basket);
+    }
+
+    @Test
+    public void testBasketInitialisableWithOneItemArray()
+    {
+        /**
+         * Test that the Basket class can be initialise with one item in it as an array
+         */
+        Item[] items = {new Item("A", 50)};
+        Basket basket = new Basket(items);
+        assertNotNull(basket);
+    }
+
+    @Test
+    public void testBasketInitialisableWithOneItemArrayList()
+    {
+        /**
+         * Test that the Basket class can be initialise with one item in it as an Array List
+         */
+        Item[] itemsA = {new Item("A", 50)};
+        ArrayList<Item> items = new ArrayList<Item>(Arrays.asList(itemsA));
+        Basket basket = new Basket(items);
+        assertNotNull(basket);
+    }
+
+    @Test
+    public void testBasketInitialisableWithSeveralItemsArray()
+    {
+        /**
+         * Test that the Basket class can be initialise withmultiple items in it as an array
+         */
+        Item[] items = {new Item("A", 50), new Item("A", 50), new Item("A", 50)};
+        Basket basket = new Basket(items);
+        assertNotNull(basket);
+    }
+
+    @Test
+    public void testBasketInitialisableWithSeveralItemsArrayList()
+    {
+        /**
+         * Test that the Basket class can be initialise withmultiple items in it as an Array List
+         */
+        Item[] itemsA = {new Item("A", 50), new Item("A", 50), new Item("A", 50)};
+        ArrayList<Item> items = new ArrayList<Item>(Arrays.asList(itemsA));
+        Basket basket = new Basket(items);
         assertNotNull(basket);
     }
     
